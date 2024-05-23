06218726268ac385957152bf443fbe9549b109e0
曹宝琪 <2024cbq@gmail.com>
2024-05-23 12:32:51 +0800

    :building_construction: build(pack): build nsis、msi、zip pack action

--- web-ui/electron/main.js
+++ web-ui/electron/main.js
@@ -1,48 +1,46 @@
-const { app, BrowserWindow, ipcMain} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('node:path')
-// 判断是否开发环境
 const isPackaged = app.isPackaged
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
-var mainWindow
+let mainWindow;
 
 app.whenReady().then(() => {
     createMainWindow()
     app.on("activate", () => {
-      if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+        if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
     })
-  })
+})
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 /**创建主窗口 */
 const createMainWindow = () => {
-  mainWindow = new BrowserWindow({
-    width: 880,
-    height: 500,
-    webPreferences:{
-      nodeIntegration:true,
-      contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
+    mainWindow = new BrowserWindow({
+        width: 880,
+        height: 500,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+
+    })
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+
+    if (!isPackaged) {
+        mainWindow.loadURL("http://localhost:5173/");
+    } else {
+        mainWindow.loadFile(path.resolve(__dirname, '../dist/index.html'))
     }
-
-  })
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
-
-
-
-  if (!isPackaged){
-    mainWindow.loadURL("http://localhost:5173/");
-  }else{
-    mainWindow.loadFile(path.resolve(__dirname, '../dist/index.html'))
-  }
 
 }
 
@@ -51,4 +49,4 @@ ipcMain.on('set-title', (event, title) => {
     const webContents = event.sender
     const win = BrowserWindow.fromWebContents(webContents)
     win.setTitle(title)
-  })
+})
